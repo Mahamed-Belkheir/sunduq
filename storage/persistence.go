861536b3fe23b5a72a1dcbd9150d845b6b4dcf1a
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-01-13 19:31:44 +0200

    added naive implementations to the Save and Load Persistence API methods

--- storage/persistence.go
+++ storage/persistence.go
@@ -1,16 +1,20 @@
 package storage
 
+import (
+	"io/ioutil"
+)
+
 //PersistenceAPI struct holds all the methods to handle storing and retrieving buckets from disk
 type PersistenceAPI struct {
 	filePath string
 }
 
 //Save saves the data into disk
-func (p *PersistenceAPI) Save() {
-
+func (p *PersistenceAPI) Save(data []byte) error {
+	return ioutil.WriteFile(p.filePath, data, 0644)
 }
 
 //Load loads the table data from disk
-func (p *PersistenceAPI) Load() {
-
+func (p *PersistenceAPI) Load() ([]byte, error) {
+	return ioutil.ReadFile(p.filePath)
 }
