be0d8611ea026f973324398d76e710af7c724581
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-01-13 20:15:13 +0200

    added Add and AddTable storage struct methods

--- storage/store.go
+++ storage/store.go
@@ -1,1 +1,45 @@
 package storage
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/Mahamed-Belkheir/sunduq/context"
+)
+
+func tableExists() error {
+	return errors.New("Table already exists")
+}
+
+func tableNotFound() error {
+	return errors.New("Table not found")
+}
+
+//Storage struct holds all the tables and holds all data manipulation methods
+type Storage struct {
+	tables map[string]Table
+	mut    *sync.RWMutex
+}
+
+//Add adds data to a specific table
+func (s Storage) Add(ctx context.Context, tableName, key string, value []byte) error {
+	defer s.mut.RUnlock()
+	s.mut.RLock()
+	table, ok := s.tables[tableName]
+	if !ok {
+		return tableNotFound()
+	}
+	return table.Add(ctx, key, value)
+}
+
+//AddTable creates and adds a new table to the storage struct
+func (s Storage) AddTable(ctx context.Context, tableName string, isSystemTable bool) error {
+	defer s.mut.Unlock()
+	s.mut.Lock()
+	_, ok := s.tables[tableName]
+	if ok {
+		return tableExists()
+	}
+	s.tables[tableName] = NewTable(tableName, ctx.User, isSystemTable)
+	return nil
+}
