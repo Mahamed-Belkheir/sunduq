964b12da5f8e9cc5eee01392626e920bdf4732f3
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-01-13 18:46:32 +0200

    added naive implementations for the table methods

--- storage/table.go
+++ storage/table.go
@@ -2,6 +2,8 @@ package storage
 
 import (
 	"errors"
+
+	"github.com/Mahamed-Belkheir/sunduq/context"
 )
 
 //AccessLevel is the base enum type for access lists
@@ -12,12 +14,14 @@ const (
 	Read AccessLevel = iota
 	//Write access allows reading and writing to tables
 	Write
+	//Admin access allows the above + editing access list and deleting tables
+	Admin
 )
 
 //IsValid verifies the access level is a valid value within the enum values
 func (a AccessLevel) IsValid() error {
 	switch a {
-	case Read, Write:
+	case Read, Write, Admin:
 		return nil
 	default:
 		return errors.New("Invalid AccessLevel Value")
@@ -45,6 +49,73 @@ func (a AccessList) canWrite(user string) bool {
 
 //Table is the basic building block for the storage, holds all the data and includes other meta data
 type Table struct {
-	ac   AccessList
-	data map[string][]byte
+	ac            AccessList
+	data          map[string][]byte
+	isSystemTable bool
+	name          string
+}
+
+//NewTable creates a new Table instance
+func NewTable(name, owner string, isSystemTable bool) Table {
+	ac := make(AccessList)
+	ac[owner] = Admin
+	return Table{
+		ac,
+		make(map[string][]byte),
+		isSystemTable,
+		name,
+	}
+}
+
+func invalidCreds() error {
+	return errors.New("User lacks the required access level")
+}
+
+func notFound() error {
+	return errors.New("Key not found")
+}
+
+//Add adds an element to the table
+func (t Table) Add(context context.Context, key string, value []byte) error {
+	if !t.ac.canWrite(context.User) {
+		return invalidCreds()
+	}
+
+	t.data[key] = value
+
+	return nil
+}
+
+//Get fetches the item from the table
+func (t Table) Get(context context.Context, key string) ([]byte, error) {
+	if !t.ac.canRead(context.User) {
+		return nil, invalidCreds()
+	}
+
+	data, ok := t.data[key]
+	if !ok {
+		return nil, notFound()
+	}
+
+	return data, nil
+}
+
+//Del deletes the item from the table
+func (t Table) Del(context context.Context, key string) error {
+	if !t.ac.canWrite(context.User) {
+		return invalidCreds()
+	}
+
+	delete(t.data, key)
+
+	return nil
+}
+
+//All fetches all data in the table
+func (t Table) All(context context.Context, key string) (map[string][]byte, error) {
+	if !t.ac.canRead(context.User) {
+		return nil, invalidCreds()
+	}
+
+	return t.data, nil
 }
