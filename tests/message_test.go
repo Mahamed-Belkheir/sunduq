0c9b64109500b2afc7039453f75c8be19e407dc7
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-01-18 22:07:47 +0200

    added result and conenct message tests

--- tests/message_test.go
+++ tests/message_test.go
@@ -2,7 +2,6 @@ package tests
 
 import (
 	"bufio"
-	"fmt"
 	"reflect"
 	"testing"
 
@@ -19,8 +18,44 @@ func TestPingSerialize(t *testing.T) {
 	}
 
 	if !reflect.DeepEqual(ping, rePing) {
-		fmt.Println(ping, rePing)
 		t.Errorf("serialized ping did not equal original ping message, \n ping: %v \n reping: %v", ping, rePing)
+	}
+}
+
+func TestResultSerialize(t *testing.T) {
+	message := "task failed successfully"
+	res := sunduq.NewResult(1, true, sunduq.String, []byte(message))
+	buf := res.ToBytesBuffer()
+	reader := bufio.NewReader(&buf)
+	reRes, err := sunduq.MessageFromBytes(reader)
+
+	if err != nil {
+		t.Errorf("result deserialization failed with error %v", err)
+	}
+
+	if !reflect.DeepEqual(res, reRes) {
+		t.Errorf("serialized result did not equal original result message, \n res: %v \n reRes: %v", res, reRes)
+	}
+
+	if message != string(reRes.Value) {
+		t.Errorf("serialized message did not match the original, \n expected: %v \n got: %v", message, string(reRes.Value))
+	}
+}
+
+func TestConnectMessage(t *testing.T) {
+	username := "bob123"
+	password := "securepassword3000"
+	con := sunduq.NewConnect(username, password)
+	buf := con.ToBytesBuffer()
+	reader := bufio.NewReader(&buf)
+	reCon, err := sunduq.MessageFromBytes(reader)
+
+	if err != nil {
+		t.Errorf("failed to parse message with error: %v", err)
+	}
+
+	if !reflect.DeepEqual(con, reCon) {
+		t.Errorf("original message did not match parsed message \n original: %v \n parsed: %v", con, reCon)
 	}
 
 }
