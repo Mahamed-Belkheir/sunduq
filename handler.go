ddebf16565c1e5aee42809dee77d42611f097f81
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-01-23 15:28:24 +0200

    add Envelope Type
    added the envelope struct that couples messages with the connection ID

--- handler.go
+++ handler.go
@@ -4,3 +4,17 @@ package sunduq
 type Handler interface {
 	Run()
 }
+
+//Envelope is a struct that holds the message and the connection ID together
+type Envelope struct {
+	ID      int
+	Message Message
+}
+
+//NewEnvelope creates a new envelope struct
+func NewEnvelope(id int, msg Message) Envelope {
+	return Envelope{
+		id,
+		msg,
+	}
+}
